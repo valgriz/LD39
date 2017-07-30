aee783bd05db893c9e0ae7ee0b9cb8b4140b029f
ngnguyennn <nnkngan011@gmail.com>
2017-07-29 20:55:07 -0400

    added graphics to webpage and made dial functional

--- js/ChargerPlayGame.js
+++ js/ChargerPlayGame.js
@@ -11,17 +11,27 @@ function checkKeys(){
 function keyPress(key){
 	if(key.keyCode === Phaser.KeyCode.UP){
 		console.log("UP");
+		charger.game.dPressed.animations.play('up');
 	} else if(key.keyCode === Phaser.KeyCode.DOWN){
 		console.log("DOWN");
+		charger.game.dPressed.animations.play('down');
 	} else if(key.keyCode === Phaser.KeyCode.LEFT){
 		console.log("LEFT");
+		charger.game.dPressed.animations.play('left');
 	} else if(key.keyCode === Phaser.KeyCode.RIGHT){
 		console.log("RIGHT");
+		charger.game.dPressed.animations.play('right');
 	} else if(key.keyCode === Phaser.KeyCode.SPACEBAR){
 		console.log("SPACEBAR");
+		charger.game.dPressed.animations.play('other');
 	} else {
 		console.log("No action defined for this key");
+		charger.game.dPressed.animations.play('other');
 	}
+}
+
+function keyRelease(key){
+	charger.game.dPressed.animations.play('other');
 }
 
 function initKeys(){
@@ -31,14 +41,27 @@ function initKeys(){
 	charger.game.key.right = charger.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
 	charger.game.key.space = charger.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 	charger.game.key.up.onDown.add(keyPress, this);
+	charger.game.key.up.onUp.add(keyRelease, this);
 	charger.game.key.down.onDown.add(keyPress, this);
+	charger.game.key.down.onUp.add(keyRelease, this);
 	charger.game.key.left.onDown.add(keyPress, this);
+	charger.game.key.left.onUp.add(keyRelease, this);
 	charger.game.key.right.onDown.add(keyPress, this);
+	charger.game.key.right.onUp.add(keyRelease, this);
 	charger.game.key.space.onDown.add(keyPress, this);
 }
 
 function initGame(){
+	charger.game.phone_background = charger.game.add.sprite(0, 0, 'phone');
+	charger.game.status_bar = charger.game.add.sprite(15, 47, 'statusBar');
+	charger.game.battery = charger.game.add.sprite(322, 52, 'battery');
 	charger.game.background = null;
+	charger.game.dPressed = charger.game.add.sprite(119, 380, 'dPressed');
+	charger.game.dPressed.animations.add('up', [1], 10 ,true);
+	charger.game.dPressed.animations.add('down', [2], 10 ,true);
+	charger.game.dPressed.animations.add('right', [3], 10 ,true);
+	charger.game.dPressed.animations.add('left', [4], 10 ,true);
+	charger.game.dPressed.animations.add('other', [0], 10 ,true);
 	charger.game.menu = null;
 	charger.game.key = [];
 	charger.game.key.up = null;
@@ -61,6 +84,9 @@ function createBackground(background_index){
 		charger.game.background.destroy();
 		charger.game.background = null;
 	}
+
+	charger.game.phone_background.moveDown();
+
 	if(background_index === 0){
 		charger.game.background = charger.game.add.sprite(15, 78, 'background');
 	}
--- js/ChargerPreload.js
+++ js/ChargerPreload.js
@@ -13,11 +13,15 @@ charger.Preload.prototype = {
 		//this.load.spritesheet('frog', 'assets/frog.png', 48, 48);
 		//this.load.spritesheet('fire', 'assets/fire.png', 32, 32);
 		this.load.image('background', 'assets/background.png');
+		this.load.image('statusBar', 'assets/statusBar.png');
+		this.load.image('phone', 'assets/phone.png');
 		this.load.image('main_menu', 'assets/main_menu.png');
 		this.load.spritesheet('you_win', 'assets/you_win.png');
 		this.load.image('you_lose', 'assets/you_lose.png');
+		this.load.spritesheet('dPressed', 'assets/dPressed.png', 194, 132);
+		this.load.image('battery', 'assets/Battery.png');
+		this.load.spritesheet('battery_bar', 'assets/batteryBar.png');
 		
-		//this.load.image('levelBackground1', 'assets/levelBackground1.png');
 	},
 	
 	create: function(){
